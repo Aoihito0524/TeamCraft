e39056cbe5e7d305b41a742040ce30288e4be62c
Aoihito <Kimukimu524@icloud.com>
2023-06-08 12:19:41 +0900

    Initial Commit

---
+++ TeamCraft/TeamCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamCraftApp.swift
+//  TeamCraft
+//
+//  Created by 木村友祐 on 2023/06/08.
+//
+
+import SwiftUI
+
+@main
+struct TeamCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
