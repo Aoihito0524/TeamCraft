e123c19d560487326ba585fe40fe63fee2c54ffe
Aoihito <Kimukimu524@icloud.com>
2023-06-20 08:40:09 +0900

    コミットテスト

--- TeamCraft/Authentication/Register/RegisterByEmailView.swift
+++ TeamCraft/Authentication/Register/RegisterByEmailView.swift
@@ -10,36 +10,32 @@ import FirebaseAuth
 
 struct RegisterByEmailView: View{
     @Binding var currentView: AuthenticationView.CurrentView
-    @State var email: String = ""
-    @State var password: String = ""
-    @State var password_reenter: String = ""
-    @State var errorOccured = false
-    @State var errorMessage = ""
+    @ObservedObject var VM = RegisterByEmailViewModel()
     var body: some View{
         VStack{
             Text("新規作成")
                 .font(.largeTitle)
                 .padding(.vertical, DEVICE_HEIGHT * 0.046)
-            if errorOccured{
-                Text(errorMessage)
+            if VM.errorOccured{
+                Text(VM.errorMessage)
             }
-            TextField("メールアドレス", text: $email)
+            TextField("メールアドレス", text: $VM.email)
                 .textFieldTexture()
                 .padding(.bottom, DEVICE_HEIGHT * 0.046)
-            PasswordField(message: "パスワード", password: $password)
+            PasswordField(message: "パスワード", password: $VM.password)
                 .textFieldTexture()
                 .padding(.bottom, DEVICE_HEIGHT * 0.046)
-            PasswordField(message: "パスワード再入力", password: $password_reenter)
+            PasswordField(message: "パスワード再入力", password: $VM.password_reenter)
                 .textFieldTexture()
                 .padding(.bottom, DEVICE_HEIGHT * 0.046)
             Button(action: {
-                if password != password_reenter{
-                    errorMessage = "同じパスワードを入力してください"
+                if VM.password != VM.password_reenter{
+                    VM.errorMessage = "同じパスワードを入力してください"
                     return;
                 }
                 //新規登録
-                Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-                    ShowError_or_Finish(error: error)
+                Auth.auth().createUser(withEmail: VM.email, password: VM.password) { authResult, error in
+                    VM.ShowError_or_Finish(error: error)
                 }
             }){
                 Text("アカウント作成")
@@ -58,6 +54,14 @@ struct RegisterByEmailView: View{
         .background(Color.white)
         .cornerRadius(10)
     }
+}
+
+class RegisterByEmailViewModel: ObservableObject{
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var password_reenter: String = ""
+    @Published var errorOccured = false
+    @Published var errorMessage = ""
     func ShowError_or_Finish(error: (any Error)?){
         if let error = error as NSError? {
             if let errorCode = AuthErrorCode.Code(rawValue: error.code){
